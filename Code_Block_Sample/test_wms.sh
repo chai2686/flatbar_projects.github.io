8c87a8300e8fed4381ba1e5abf590a26359240f7
Thawatchai Panyapatthanakit <52881563+chai2686@users.noreply.github.com>
2026-06-28 14:50:05 +0700

    Update test_wms.sh

--- Code_Block_Sample/test_wms.sh
+++ Code_Block_Sample/test_wms.sh
@@ -3,7 +3,7 @@
 FILE_PATH="/tmp/preparing_data.txt"
 RESPONSE_FILE="/tmp/execute_response_test.txt"
 
-TARGET_URL="https://script.google.com/macros/s/AKfycbwFC017VjXqTtng6NGslhs4qoglvLvWuR0GxGQy-tGB54Ht0x-Nm8zoxfZ1Avb2hv6GaA/exec"
+TARGET_URL="https://script.google.com/macros/s/AKfycbyHukT2sYQ5XRrOf7DgJ0YaaA5O0dIsebw4i-OgCcRTvCQHkwAvNiNWAM0sY9ephlfZJQ/exec"
 
 HTTP_STATUS=$(curl -s -X POST "$TARGET_URL" \
     -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
@@ -12,7 +12,7 @@ HTTP_STATUS=$(curl -s -X POST "$TARGET_URL" \
     -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36 Edg/137.0.0.0' \
     --data-urlencode "action=apiprocess" \
     --data-urlencode "api=PREPARING_K8" \
-    --data-urlencode "apicontent=@$FILE_PATH" \
+    --data-urlencode "apicontent=$(<"$FILE_PATH")" \
     --insecure \
     -w "%{http_code}" \
-    -o "$RESPONSE_FILE")
+    )
